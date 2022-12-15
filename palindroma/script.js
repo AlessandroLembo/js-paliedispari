41c168be0f643c1fb54b8932ee10de1d4b3318a1
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-15 15:57:36 +0100

    modifica analisi più primo controllo

--- palindroma/script.js
+++ palindroma/script.js
@@ -12,11 +12,12 @@ ANALISI
 
 1 - Prendo gli elementi dalla pagina.
 2 - Aggancio l'event listener al button.
-3 - Inserisco una parola nel campo.
-4 - Dichiaro una funzione in cui sviluppo la logica. 
-5 - Controllo se ho inserito almeno 2 caratteri e che non siano numeri.
-6 - Controllo se la parola inserita sia palindroma.
-7 - Stampo il messaggio in pagina. 
+3 - Inserisco una parola nel campo e la salvo in variabile.
+4 - Controllo se ho inserito almeno 2 caratteri e che non siano numeri.
+5 - Dichiaro una funzione in cui sviluppo la logica. 
+6 - Dichiaro una variabile con valore booleano 'false'.
+7 - Controllo se la parola inserita sia palindroma.
+8 - Stampo il messaggio in pagina. 
 */
 
 // 1 - Prendo gli elementi dalla pagina.
@@ -24,3 +25,31 @@ const wordElement = document.getElementById('word');
 const buttonElement = document.getElementById('button');
 const messageElement = document.getElementById('message');
 // console.log(wordElement, buttonElement, messageElement);
+
+
+// 2 - Aggancio l'event listener al button
+buttonElement.addEventListener('click', function(){
+    
+
+    // 3 - Inserisco una parola nel campo e la salvo in variabile.
+    const valueElement = wordElement.value.trim();
+    // console.log(valueElement);
+
+    // 4 - Controllo se ho inserito almeno 2 caratteri e che non siano numeri.
+    if (!isNaN(valueElement) || valueElement.length < 2){
+        console.log ('Non inserire numeri e inserisci almeno 2 caratteri');
+    }
+    
+    // 5 - Dichiaro una funzione in cui sviluppo la logica.
+    function isPalindrome(word){   
+
+       // 6 -  Dichiaro una variabile con valore booleano 'false'.
+       let result = false;
+
+       
+       return result;
+    }
+       
+})
+
+
