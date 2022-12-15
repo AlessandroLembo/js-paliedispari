faabfaaa0aa4dc2c77cd87e49cf69dcd77e4b2c1
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-15 14:57:07 +0100

    Analisi esercizio Palindroma

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,1 +1,20 @@
 console.log(' JS OK');
+
+/*
+TRACCIA: Palindroma
+
+Chiedere all’utente di inserire una parola tramite un form
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+/* 
+ANALISI
+
+1 - Prendo gli elementi dalla pagina.
+2 - Aggancio l'event listener al button.
+3 - Inserisco una parola nel campo.
+4 - Dichiaro una funzione in cui sviluppo la logica. 
+5 - Controllo se ho inserito almeno 2 caratteri e che non siano numeri.
+6 - Controllo se la parola inserita sia palindroma.
+7 - Stampo il messaggio in pagina. 
+*/
