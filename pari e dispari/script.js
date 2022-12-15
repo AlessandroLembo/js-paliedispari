54a09bd853e89af46951ca034a2bc229ad5c6829
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-15 17:41:03 +0100

    Analisi esercizio pari e dispari

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,1 +1,24 @@
 console.log(' JS OK ');
+
+/*
+TRACCIA: Pari e Dispari
+
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5 tramite un form.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+/*
+ANALISI: 
+
+1 - Prendo gli elementi dalla pagina.
+2 - Aggancio l'event listener al button.
+3 - Inserisco un numero nel campo e lo salvo in variabile.
+4 - Controllo se ho inserito un numero e che sia compreso tra 1 e 5.
+5 - Dichiaro una funzione per generare un numero random per il computer.
+6 - Sommiamo i due numeri.
+7 - Dichiaro una funzione per controllare se la somma è pari o dispari. 
+8 - Stampo in pagina il messaggio.
+*/
