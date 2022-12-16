1cdeb1a1aa242e59aad1c73adaf3f03bf2748621
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-16 21:06:13 +0100

    calcolo somma

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -14,57 +14,54 @@ Dichiariamo chi ha vinto.
 ANALISI: 
 
 1 - Prendo gli elementi dalla pagina.
-2 - Aggancio l'event listener al button per inserire il numero dell'utente.
+2 - Aggancio l'event listener al button.
 3 - Inserisco un numero nel campo e lo salvo in variabile.
 4 - Controllo se ci sono caratteri non numerici e se i numeri sono compresi tra 1 e 5.
-5 - Aggancio l'event listener al button per generare un numero random per il pc.
-6 - Dichiaro una funzione per generare un numero random per il computer.
-7 - Aggancio l'event listener al button per calcolare.
-8 - Dichiaro una funzione per controllare se la somma è pari o dispari. 
-9 - Stampo in pagina il messaggio.
+5 - Dichiaro una funzione per generare un numero random per il computer.
+6 - Dichiaro una funzione per controllare se la somma è pari o dispari. 
+7 - Stampo in pagina il messaggio.
 */
 
 // 1 - Prendo gli elementi dalla pagina.
 const oddEvenElement = document.getElementById('odd-even');
 const userNumberElement = document.getElementById('user-number');
-const buttonUserChoise = document.getElementById('button-user');
-const buttonRandomNumber = document.getElementById('button-random');
 const buttonSumNumbers = document.getElementById('button-sum');
 const numberUserElement = document.getElementById('number-user');
 const numberPcElement = document.getElementById('number-pc');
 const messageElement = document.getElementById('message');
-// console.log(oddEvenElement, userNumberElement, buttonUserChoise, buttonRandomNumber, buttonSumNumbers, numberUserElement, numberPcElement, messageElement);
+// console.log(oddEvenElement, userNumberElement, buttonSumNumbers, numberUserElement, numberPcElement, messageElement);
 
 
-// 2 - Aggancio l'event listener al button per inserire il numero dell'utente.
-buttonUserChoise.addEventListener('click', function(){
+
+// 2 - Aggancio l'event listener al button.
+buttonSumNumbers.addEventListener('click', function(){
 
     // 3 - Inserisco un numero nel campo e lo salvo in variabile.
-    const userChoise =  userNumberElement.value.trim();
+    const userNumberChoise =  parseInt(userNumberElement.value.trim());
+    const userOddEvenChoise = oddEvenElement.value;
 
     /* 4 - Controllo se ci sono caratteri non numerici
            e se i numeri sono compresi tra 1 e 5.*/
-    let userNumber = "è dispari";   
+    
+    if (isNaN(userNumberChoise) || userNumberChoise < 1 || userNumberChoise > 5) {
+    alert('Inserisci un numero compreso tra 1 e 5');
+    return;
+    }  
 
-    if (userNumberElement.value < 1 || userNumberElement.value > 5) {
-      alert('Inserisci un numero compreso tra 1 e 5');
-    }  else if (userNumberElement.value % 2 === 0){
-        userNumber = "è pari";
-    } 
+    let userNumber = "è dispari";
 
-    const resultUser = `Il numero scelto dall'utente è ${userNumberElement.value} ed è ${userNumber}`;
+    if (userNumberChoise % 2 === 0){
+         userNumber = "è pari";
+    }
+
+    const resultUser = `Il numero scelto dall'utente è ${userNumberChoise} ed ${userNumber}`;
 
     console.log(resultUser);
     numberUserElement.innerHTML = resultUser;
-        
 
-    // 5 - Aggancio l'event listener al button per generare un numero random per il pc.
-    buttonRandomNumber.addEventListener('click', function(){
-    
-    // 6 - Dichiaro una funzione per generare un numero random per il computer.
+    // 5 - Dichiaro una funzione per generare un numero random per il computer.
     function getRandomNumber() {
         const randomNumber = Math.floor(Math.random() * 5) + 1;
-
 
         let pcNumber = "è dispari";
 
@@ -72,7 +69,7 @@ buttonUserChoise.addEventListener('click', function(){
             pcNumber = "è pari";
         }
         
-        const resultPc = `Il numero generato dal Pc è ${randomNumber} ed è ${pcNumber}`;
+        const resultPc = `Il numero generato dal Pc è ${randomNumber} ed ${pcNumber}`;
         console.log(resultPc);
         numberPcElement.innerHTML = resultPc;
 
@@ -81,10 +78,11 @@ buttonUserChoise.addEventListener('click', function(){
 
     const resultPc = getRandomNumber();
     
-   
-    
-      
-})
+    const sum = resultPc + userNumberChoise;
+    console.log(sum);
 
+
+    
+    
 
 })
